6f8c2eddf5ef5f95e92dfec650ff995b04b3dd63
Abdulkadir Ahmad <aabdulkadir109@gmail.com>
2023-05-19 23:25:10 -0700

    air conditioning page done

---
+++ air-conditioning.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<?php 
+include 'header.php';
+?>
+
+	<section class="pager-sec">
+		<div class="container">
+			<ul class="pager-info">
+				<li> <a href="index.php" title="">Home</a></li>
+				<li><a href="services.php" title="">Services</a></li>
+                <li><a title="">Air Conditioning</a></li>
+             
+			</ul><!--pager-info end-->
+		</div>
+	</section><!---pager-sec end-->
+
+
+	<div class="page-content p-0">
+
+		<section class="block tp-100">
+			<div class="container">
+				<div class="service_single_page">
+					<div class="row">
+						<div class="col-lg-9 col-md-9 pull-right">
+							<div class="service-single-details">
+								<div class="service-single">
+									<h2>Air Conditioning</h2>
+									<img src="images/resources/service-img.jpg" alt="">
+									<h3>High Efficient Ac Installation, A/c Installation & Air Conditioning Installation</h3>
+									<p>Central air conditioning allows you to enjoy the perfect temperature in any room of your house. Bedrooms can be kept refreshingly cool even on the stickiest summer nights. You’ll benefit from ideal sleeping conditions, complete humidity control, and a fresher, cleaner home. Through effective, air-purifying filtration systems, cooling units also reduce bacteria, dust, house mites, mold, pet dander and pollen, proving extremely beneficial for those who suffer from allergies and respiratory problems. </p>
+									<p> When you’re ready to experience your summer in a whole new way, call Omzfield for air conditioning installation, and we’ll exceed your expectations.</p>
+									<h3>Expert AC Replacement</h3>
+									<p>At Omzfield we are committed to doing the job right. As a Factory Authorized Dealer, our technicians are NATE-certified professionals who have completed a demanding set of core and specialty exams to ensure their technical competency. They also continue training specific to Carrier technology and products. We are fully bonded and insured, offer free in-home estimates and convenient financing options.  As family owned and operated business since 2010, we’ve served the comfort needs of Pittsburgh and the surrounding areas with a dedication to providing the best prices for the very best service.</p>
+									<p> When it comes to managing indoor humidity, the two-stage compressor makes a significant comfort difference, allowing the system to function mostly in low-stage, where you achieve the highest efficiency.  A filter drier system protects against moisture and contaminants while compressor sound blanket keeps operational noise down to 65 decibels.</p>
+									<h3>Top Quality Air Conditioning Installation</h3>
+									<p>By installing an air conditioning system that has met the standards set by the Environmental Protection Agency, you stand to save 30% on cooling bills each year. </p>
+									<p>The factory-trained technicians from Omzfield have the experience, product knowledge and commitment to design a system to perfectly match your expectations for efficiency, performance and budget. Through skilled and meticulous installation, our team will get the most out of your energy dollar, allowing you to maintain luxurious indoor temperatures without suffering the consequences of high utility bills.  We not only take pride in completing your project quickly, neatly and perfectly, we’re available for seasonal maintenance to keep your system operating at peak capacity. </p>
+								</div><!--service-single end-->
+								<div class="service-svv">
+									<div class="svv-img">
+										<img src="images/resources/vc.png" alt="">
+									</div>
+									<div class="svv-info">
+										<h3>Peace of mind for you and your family.</h3>
+										<p>Omzfield has been repairing AC systems for more than 10 years. We want you to be cool and comfortable, and we’ll guarantee any repairs we make through a one-year warranty.</p>
+									</div><!--svv-info end-->
+								</div><!--service-svv end-->
+							</div>
+						</div>
+						<div class="col-lg-3 col-md-3 ful_wd pull-left">
+							<div class="side_bar">
+								<div class="dvis">
+									<ul class="tabs_list">
+										<li data-tab="coo_ling">
+											<span class="tc_icon">
+												<svg version="1.1" id="Capa_5" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+													 viewBox="0 0 333.316 333.316" xml:space="preserve">
+													<path d="M315.797,224.574l-5.557-20.818l-39.684,10.592l-33.572-19.441l41.764-18.377v-19.75l-41.767-18.377l33.572-19.439
+													l39.689,10.59l5.557-20.818l-18.861-5.025l19.361-11.197l-10.82-18.645l-19.324,11.188l5.029-18.855l-20.818-5.551l-10.588,39.674
+													l-33.473,19.338l4.918-45.148l-17.061-9.877l-36.758,26.892V52.824l29.045-29.018L191.221,8.539l-13.816,13.807V0h-21.531v22.346
+													L142.064,8.539l-15.236,15.268l29.045,29.018v38.703l-36.756-26.892l-17.061,9.877l4.924,45.148l-33.477-19.336l-10.59-39.676
+													l-20.816,5.553l5.027,18.854l-19.32-11.188L17.018,92.512l19.322,11.197l-18.857,5.025l5.557,20.818l39.684-10.59l33.576,19.439
+													l-41.771,18.377v19.75l41.768,18.377l-33.576,19.441l-39.678-10.592l-5.557,20.818l18.853,5.025l-19.32,11.195l10.787,18.652
+													l19.318-11.18l-5.029,18.84l20.818,5.559l10.59-39.682l33.479-19.338l-4.924,45.15l17.061,9.879l36.756-26.893V280.5l-29.045,29.004
+													l15.236,15.266l13.809-13.805v22.352h21.531v-22.352l13.816,13.805l15.229-15.266L177.404,280.5v-38.717l36.758,26.893l17.061-9.879
+													l-4.92-45.15l33.475,19.338l10.59,39.682l20.818-5.559l-5.029-18.838l19.322,11.178l10.82-18.652l-19.361-11.193L315.797,224.574z
+													 M247.668,166.654l-33.779,14.885l-25.75-14.887l25.746-14.877L247.668,166.654z M207.148,96.471l-3.975,36.578l-25.77,14.93
+													v-29.721L207.148,96.471z M126.129,96.471l29.744,21.787v29.721l-25.771-14.93L126.129,96.471z M85.609,166.654l33.783-14.879
+													l25.748,14.877l-25.752,14.887L85.609,166.654z M126.129,236.84l3.973-36.582l25.771-14.924v29.717L126.129,236.84z M207.148,236.84
+													l-29.744-21.789v-29.717l25.77,14.924L207.148,236.84z"/>
+												</svg>
+											</span>
+											<span class="svs">Cooling Services</span>
+										</li>
+										<li class="active heating" data-tab="hea_ting">
+											<span class="tc_icon">
+												<svg version="1.1" id="Capa_6" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+													 width="45.16px" height="45.16px" viewBox="0 0 45.16 45.16" xml:space="preserve"
+													>
+												<g>
+													<g>
+														<path d="M22.58,11.269c-6.237,0-11.311,5.075-11.311,11.312s5.074,11.312,11.311,11.312c6.236,0,11.311-5.074,11.311-11.312
+															S28.816,11.269,22.58,11.269z"/>
+														<g>
+															<g>
+																<path d="M22.58,7.944c-1.219,0-2.207-0.988-2.207-2.206V2.207C20.373,0.988,21.361,0,22.58,0c1.219,0,2.207,0.988,2.207,2.207
+																	v3.531C24.787,6.956,23.798,7.944,22.58,7.944z"/>
+															</g>
+															<g>
+																<path d="M22.58,37.215c-1.219,0-2.207,0.988-2.207,2.207v3.53c0,1.22,0.988,2.208,2.207,2.208c1.219,0,2.207-0.988,2.207-2.208
+																	v-3.53C24.787,38.203,23.798,37.215,22.58,37.215z"/>
+															</g>
+															<g>
+																<path d="M32.928,12.231c-0.861-0.862-0.861-2.259,0-3.121l2.497-2.497c0.861-0.861,2.259-0.861,3.121,0
+																	c0.862,0.862,0.862,2.26,0,3.121l-2.497,2.497C35.188,13.093,33.791,13.093,32.928,12.231z"/>
+															</g>
+															<g>
+																<path d="M12.231,32.93c-0.862-0.863-2.259-0.863-3.121,0l-2.497,2.496c-0.861,0.861-0.862,2.26,0,3.121
+																	c0.862,0.861,2.26,0.861,3.121,0l2.497-2.498C13.093,35.188,13.093,33.79,12.231,32.93z"/>
+															</g>
+															<g>
+																<path d="M37.215,22.58c0-1.219,0.988-2.207,2.207-2.207h3.531c1.219,0,2.207,0.988,2.207,2.207c0,1.219-0.988,2.206-2.207,2.206
+																	h-3.531C38.203,24.786,37.215,23.799,37.215,22.58z"/>
+															</g>
+															<g>
+																<path d="M7.944,22.58c0-1.219-0.988-2.207-2.207-2.207h-3.53C0.988,20.373,0,21.361,0,22.58c0,1.219,0.988,2.206,2.207,2.206
+																	h3.531C6.956,24.786,7.944,23.799,7.944,22.58z"/>
+															</g>
+															<g>
+																<path d="M32.928,32.93c0.862-0.861,2.26-0.861,3.121,0l2.497,2.497c0.862,0.86,0.862,2.259,0,3.12s-2.259,0.861-3.121,0
+																	l-2.497-2.497C32.066,35.188,32.066,33.791,32.928,32.93z"/>
+															</g>
+															<g>
+																<path d="M12.231,12.231c0.862-0.862,0.862-2.259,0-3.121L9.734,6.614c-0.862-0.862-2.259-0.862-3.121,0
+																	c-0.862,0.861-0.862,2.259,0,3.12l2.497,2.497C9.972,13.094,11.369,13.094,12.231,12.231z"/>
+															</g>
+														</g>
+													</g>
+												</g>
+												</svg>
+											</span>
+											<span class="svs">Heating Services</span>
+										</li>
+									</ul><!--tabs_list end-->
+									<div class="sdbr-list">
+										<ul class="wegts-links" id="coo_ling">
+											<li>Cooling Installation</li>
+											<li>Heating Repair</li>
+											<li>Furnaces</li>
+											<li>Heat Pumps</li>
+											<li>Ductwork Installation</li>
+											<li>24/7 Emergency</li>
+										</ul>
+										<ul class="wegts-links active" id="hea_ting">
+											<li>Heating Installation</li>
+											<li>Heating Repair</li>
+											<li>Furnaces</li>
+											<li>Heat Pumps</li>
+											<li>Ductwork Installation</li>
+											<li>24/7 Emergency</li>
+										</ul>
+									</div><!--sdbr-list end-->
+								</div><!--dvis end-->
+								<div class="contact_wd">
+									<h3>Our Contacts</h3>
+									<ul>
+										<li>
+											<span><i class="fa fa-map-marker"></i></span>
+											<h4>No 25 Lobito Crescent Wuse II Abuja</h4>
+										</li>
+										<li>
+											<span><i class="fa fa-clock-o"></i></span>
+											<h4>Monday-Friday <br /> 8:00 am - 8:00 pm</h4>
+										</li>
+										<li>
+											<span><i class="fa fa-phone"></i></span>
+											<h4>24/7 Emergency Service: +234 803 449 6428</h4>
+										</li>
+									</ul>
+								</div><!--contact_wd end-->
+								<div class="ht_form">
+									<h3>Ask Question</h3>
+									<form id="contactform" name="contactform" method="post" novalidate>
+										<div id="success">
+											<p>Your message was sent successfully!</p>
+										</div>
+										<div id="error">
+											<p>Something went wrong, try refreshing and submitting the form again.</p>
+										</div>
+										<div class="input_field">
+											<input type="text" name="name" placeholder="Your name">
+										</div>
+										<div class="input_field">
+											<input type="text" name="email" placeholder="Your e-mail">
+										</div>
+										<div class="input_field">
+											<textarea name="message" placeholder="Your question"></textarea>
+										</div>
+										<div class="submit_btn">
+											<input type="submit" id="submit" name="submit" value="Ask Question">
+										</div>
+									</form>
+								</div><!--ht_form end-->
+							</div><!--side_bar end-->
+						</div>
+					</div>
+				</div><!--service_single_page end-->
+			</div>
+		</section>
+
+	</div><!--page-content end-->
+
+	<section class="newsletter-sec">
+		<div  class="container">
+			<ul class="nss_list">
+				<li>
+					<span>24/Hour</span> Emergency Service
+				</li>
+				<li>
+					<span>Free</span> Consultations
+				</li>
+				<li>
+					Newsletter Subscribe
+				</li>
+			</ul><!--nss_list end-->
+			<form class="newsletter_form" id="newsletter_form" method="post" novalidate="novalidate">
+				<input type="email" name="email" placeholder="Your e-mail" class="valid">
+				<input type="submit" name="submit" value="OK">
+			</form>
+		</div>
+	</section><!--newsletter-sec end-->
+
+	<div class="our_map v1">
+		<div class="mapouter">
+		<iframe id="gmap_canvas4" src="https://maps.google.com/maps?q=No%2025%20Lobito%20Crescent%20Wuse%20II%20Abuja,%20Nigeria&t=&z=13&ie=UTF8&iwloc=&output=embed"></iframe>
+		</div>
+	</div><!--our_map end-->
+<?php include 'footer.php';?>
